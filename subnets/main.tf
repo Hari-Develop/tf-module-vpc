55d1320c90ebafa3b7ef1bbd033c41a484816b4d
harinadhareddy <yadalaharinadhareddy01@gmail.com>
2023-08-20 21:48:22 +0530

    file changes

--- subnets/main.tf
+++ subnets/main.tf
@@ -1,5 +1,6 @@
 resource "aws_subnet" "main" {
+  count = length(var.cidr_block)
   vpc_id     = var.vpc_id
-  cidr_block = var.cidr_block
+  cidr_block = var.cidr_block[count.index]
   tags = merge( var.tags , {Name = "${var.env}-subnet-${var.name}-${count.index+1}"})
 }
